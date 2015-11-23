af5e945dba60057c5fd47d22d1666b7566abfb84
Ji ZHANG <zhangji87@gmail.com>
2015-11-23 22:01:35 +0800

    update event

--- src/main/resources/static/js/event.js
+++ src/main/resources/static/js/event.js
@@ -62,19 +62,9 @@ Event.prototype = {
 			},
 
 			eventClick: function(event) {
-				if (confirm('Delete this event?')) {
-
-					var data = {
-						id: event.id
-					};
-
-					$.post(self.contextPath + '/event/delete', data, function(result) {
-						if (result.status != 'ok') {
-							alert(result.msg);
-						}
-						$('#calendar').fullCalendar('removeEvents', event.id);
-					});
-				}
+				$('#txtTitle').val(event.title);
+				$('#dlgEvent').data('event', event);
+				$('#dlgEvent').dialog('open');
 			},
 
 			eventDrop: function(event, delta, revertFunc) {
@@ -89,6 +79,62 @@ Event.prototype = {
 		$(window).resize(function() {
 			$('#calendar').fullCalendar('option', 'height', self.calcHeight());
 		});
+
+		$('#dlgEvent').dialog({
+			autoOpen: false,
+			width: 400,
+			height: 300,
+			modal: true,
+			buttons: {
+				'Save': function() {
+
+					var event = $('#dlgEvent').data('event');
+					var title = $('#txtTitle').val();
+
+					if (title) {
+
+						var oldTitle = event.title;
+						event.title = title;
+						$('#calendar').fullCalendar('updateEvent', event);
+
+						self.saveEvent(event, function() {
+							event.title = oldTitle;
+							$('#calendar').fullCalendar('updateEvent', event);
+						});
+
+					}
+
+					$('#dlgEvent').dialog('close');
+
+				},
+				'Delete': function() {
+
+					var event = $('#dlgEvent').data('event');
+
+					if (confirm('Delete this event?')) {
+
+						var data = {
+							id: event.id
+						};
+
+						$.post(self.contextPath + '/event/delete', data, function(result) {
+							if (result.status != 'ok') {
+								alert(result.msg);
+							}
+							$('#calendar').fullCalendar('removeEvents', event.id);
+						});
+
+					}
+
+					$('#dlgEvent').dialog('close');
+
+				},
+				'Cancel': function() {
+					$('#dlgEvent').dialog('close');
+				}
+			}
+		});
+
 	},
 
 	saveEvent: function(event, revertFunc) {
@@ -103,7 +149,7 @@ Event.prototype = {
 
 		$.post(self.contextPath + '/event/save', data, function(result) {
 			if (result.status != 'ok') {
-				alsert(result.msg);
+				alert(result.msg);
 				revertFunc();
 				return;
 			}
