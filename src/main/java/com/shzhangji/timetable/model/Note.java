e951252f818fe86f4b8e50fc808374ca09588a69
Ji ZHANG <zhangji87@gmail.com>
2015-12-06 11:04:52 +0800

    fix data type

--- src/main/java/com/shzhangji/timetable/model/Note.java
+++ src/main/java/com/shzhangji/timetable/model/Note.java
@@ -2,6 +2,7 @@ package com.shzhangji.timetable.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,6 +15,7 @@ public class Note {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	@Column(columnDefinition = "TEXT")
 	private String content;
 	private Date created;
 }
