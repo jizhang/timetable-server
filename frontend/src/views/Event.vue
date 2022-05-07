8c3954f81708d86c38e597ca5c2953ccf8ce96a3
Ji ZHANG <zhangji87@gmail.com>
2022-05-07 20:00:07 +0800

    Add fullcalendar

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -1,6 +1,27 @@
-<setup setup lang="ts">
-</setup>
+<script setup lang="ts">
+import '@fullcalendar/core/vdom'
+import FullCalendar from '@fullcalendar/vue3'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+const options = {
+  plugins: [timeGridPlugin, interactionPlugin],
+  nowIndicator: true,
+  selectable: true,
+  allDaySlot: false,
+  firstDay: 1,
+  scrollTime: '08:00:00',
+}
+</script>
 
 <template>
-  <div>hello world</div>
+  <div style="width: 1000px">
+    <FullCalendar :options="options" />
+  </div>
 </template>
+
+<style>
+tr {
+  height: 30px;
+}
+</style>
