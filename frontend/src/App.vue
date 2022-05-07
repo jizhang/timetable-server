560ed4f6b338b22ef24a4ee0b0aae1408bda8298
Ji ZHANG <zhangji87@gmail.com>
2022-05-07 20:06:04 +0800

    Add fullcalendar (#21)
    * Update .gitignore
    
    * Add resolve alias
    
    * Add fullcalendar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Event from '@/views/Event.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Event />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+  color: #303133;
+  font-size: 14px;
 }
 </style>
