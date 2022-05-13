7ffda2ead70f4ef66f9ab6ad1490197f4b1243d1
Ji ZHANG <zhangji87@gmail.com>
2022-05-14 06:44:51 +0800

    Create mock middleware

--- frontend/.proxyrc.js
+++ frontend/.proxyrc.js
@@ -1,49 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
-const chokidar = require('chokidar')
-const glob = require('glob')
-const Router = require('routes')
-
-function setupWatcher() {
-  const watcher = chokidar.watch('./mock', {
-    ignoreInitial: true,
-  })
-
-  watcher.on('all', () => {
-    console.log('Reload mocks.')
-    Object.keys(require.cache)
-      .filter((id) => /[\/\\]mock[\/\\]/.test(id))
-      .forEach((id) => {
-        delete require.cache[id]
-      })
-  })
-}
-
-function createRouter() {
-  const router = new Router()
-
-  glob.sync('./mock/**/*.js').forEach((file) => {
-    const routes = require(file)
-    Object.keys(routes).forEach((path) => {
-      router.addRoute(path, routes[path])
-    })
-  })
-
-  return router
-}
-
-function createMockMiddleware() {
-  setupWatcher()
-
-  return (req, res, next) => {
-    const router = createRouter()
-    const m = router.match(req.method + ' ' + req.url)
-    if (m) {
-      m.fn(req, res, m.params)
-    } else {
-      next()
-    }
-  }
-}
+const { createMockMiddleware } = require('./mock-middleware')
 
 module.exports = function (app) {
   if (process.env.MOCK === 'none') {
@@ -52,6 +8,6 @@ module.exports = function (app) {
     })
     app.use(proxy)
   } else {
-    app.use(createMockMiddleware())
+    app.use(createMockMiddleware('./mock'))
   }
 }
