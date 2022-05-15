2853ad9c77c3e95795a18dd3e0ddd3a9ff77f5c1
Ji ZHANG <zhangji87@gmail.com>
2022-05-15 14:37:00 +0800

    Move api spec into dedicated file

--- timetable/commands/gen.py
+++ timetable/commands/gen.py
@@ -1,30 +1,11 @@
 from pathlib import Path
 
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec_webframeworks.flask import FlaskPlugin
-
 from timetable import app
-from timetable.schemas.note import NoteFormSchema, NoteSchema
-from timetable.views.note import save_note
+from timetable.openapi import spec
 
 
 @app.cli.command()
 def gen():
-    spec = APISpec(
-        title='Timetable',
-        version='0.1.0',
-        openapi_version='3.0.2',
-        info={'description': 'What have you done today?'},
-        servers=[{'url': 'http://localhost:1234'}],
-        plugins=[FlaskPlugin(), MarshmallowPlugin()]
-    )
-
-    spec.components.schema('NoteForm', schema=NoteFormSchema)
-    spec.components.schema('Note', schema=NoteSchema)
-
-    spec.path(view=save_note)
-
     spec_path = Path(__file__).parent.joinpath('../../openapi.yaml').resolve()
     with open(spec_path, 'w') as f:
         f.write('# DO NOT EDIT\n')
