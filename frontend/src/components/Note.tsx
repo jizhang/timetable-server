4daeec63351c97d7d505212d7d51d21803a459b6
Ji ZHANG <zhangji87@gmail.com>
2022-05-21 11:55:44 +0800

    Use react-hook-form

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,43 +1,61 @@
 import * as dayjs from 'dayjs'
 import { useState } from 'react'
-import { useQuery } from 'react-query'
-import { NoteApi } from '~/src/openapi'
+import { useQuery, useMutation } from 'react-query'
+import { useForm } from 'react-hook-form'
+import { NoteApi, type SaveNoteRequest } from '~/src/openapi'
 import * as styles from './Note.module.css'
 
 const noteApi = new NoteApi()
 let saveHandler
 
 export default function() {
-  const [content, setContent] = useState<string | null>(null)
   const [created, setCreated] = useState('')
 
-  const { data } = useQuery('note', () => noteApi.getNoteContent())
+  const { data } = useQuery('note', () => noteApi.getNoteContent(), {
+    initialData: { content: '' },
+  })
+
+  const { mutate, isLoading } = useMutation(
+    (values: SaveNoteRequest) => {
+      return noteApi.saveNote(values)
+    },
+    {
+      onSuccess: (data) => {
+        setCreated(dayjs(data.created).format('YYYY-MM-DD HH:mm:ss'))
+      },
+    }
+  )
+
+  const { register, handleSubmit } = useForm()
 
   function handleChangeContent(event) {
-    setContent(event.target.value)
     clearTimeout(saveHandler)
     saveHandler = setTimeout(() => {
-      handleSaveContent()
+      handleSubmit(handleSaveContent)()
     }, 5000)
   }
 
-  function handleSaveContent() {
+  function handleSaveContent(values) {
     clearTimeout(saveHandler)
-    noteApi.saveNote({ content }).then(response => {
-      setCreated(dayjs(response.created).format('YYYY-MM-DD HH:mm:ss'))
-    })
+    mutate(values)
   }
 
   return (
-    <>
+    <form onSubmit={handleSubmit(handleSaveContent)}>
       <div className={styles.title}>Note</div>
       <div>
-        <textarea className={styles.content} value={content ?? data?.content} onChange={handleChangeContent} />
+        <textarea
+          {...register('content', { onChange: handleChangeContent })}
+          className={styles.content}
+          defaultValue={data.content}
+        />
       </div>
-      <input type="button" value="Save" onClick={handleSaveContent} />
+      <div>
+        <input type="submit" value="Save" disabled={isLoading} />
+      </div>
       {created && (
         <div>Saved {created}</div>
       )}
-    </>
+    </form>
   )
 }
