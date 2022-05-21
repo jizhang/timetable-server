40c612f6c4e48f882804394b5a68ffd6d0461803
Ji ZHANG <zhangji87@gmail.com>
2022-05-21 22:51:18 +0800

    Add eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', 'openapi'],
+  overrides: [
+    {
+      files: ['*.js'],
+      env: {
+        node: true,
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+}
