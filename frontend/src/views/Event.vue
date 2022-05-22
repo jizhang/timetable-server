5180d28f92547e3c4310e61b91a6f8043401311d
Ji ZHANG <zhangji87@gmail.com>
2022-05-23 07:22:33 +0800

    Fix event view

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -3,25 +3,42 @@ import '@fullcalendar/core/vdom'
 import FullCalendar from '@fullcalendar/vue3'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import Note from '@/components/Note.vue'
 
 const options = {
-  plugins: [timeGridPlugin, interactionPlugin],
-  nowIndicator: true,
-  selectable: true,
   allDaySlot: false,
   firstDay: 1,
+  nowIndicator: true,
+  plugins: [timeGridPlugin, interactionPlugin],
   scrollTime: '08:00:00',
+  selectable: true,
 }
 </script>
 
 <template>
-  <div style="width: 1000px">
-    <FullCalendar :options="options" />
+  <div>
+    <div class="calendar">
+      <FullCalendar :options="options" />
+    </div>
+
+    <div class="note">
+        <Note />
+      </div>
   </div>
 </template>
 
 <style>
-tr {
+.calendar {
+  width: 1000px;
+}
+
+.calendar tr {
   height: 30px;
+}
+
+.note {
+  position: absolute;
+  left: 1024px;
+  top: 10px;
 }
 </style>
