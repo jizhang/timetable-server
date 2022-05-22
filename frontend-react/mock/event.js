9172b8c85f9be035ae43ba30f42c0a67baf07f8f
Ji ZHANG <zhangji87@gmail.com>
2022-05-23 07:59:13 +0800

    Remove react

--- frontend-react/mock/event.js
+++
@@ -1,9 +0,0 @@
-const sendJson = require('send-data/json')
-
-function ping(req, res) {
-  sendJson(req, res, 'pong')
-}
-
-module.exports = {
-  'GET /event/ping': ping,
-}
--- frontend-react/src/openapi/models/index.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Note';
-export * from './NoteForm';
