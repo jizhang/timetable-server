c2dcdcb8e8eb81deb1e3d817cb2c70a9047dfcdb
Ji ZHANG <zhangji87@gmail.com>
2022-05-28 21:48:59 +0800

    Add bootstrap v5

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { Modal } from 'bootstrap'
 import '@fullcalendar/core/vdom'
 import FullCalendar, { CalendarOptions } from '@fullcalendar/vue3'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import Note from '@/components/Note.vue'
+
+const modalRef = ref<HTMLElement | null>(null)
+let modal: Modal
+
+onMounted(() => {
+  if (modalRef.value !== null) {
+    modal = new Modal(modalRef.value, {
+      backdrop: 'static',
+    })
+  }
+})
+
+function saveEvent() {
+  console.log('save')
+  modal.hide()
+}
 
 const options: CalendarOptions = {
   allDaySlot: false,
@@ -18,6 +36,7 @@ const options: CalendarOptions = {
 
   select({ start, end }) {
     console.log(start, end)
+    modal.show()
   },
 
   eventClick({ event }) {
@@ -41,8 +60,25 @@ const options: CalendarOptions = {
     </div>
 
     <div class="note">
-        <Note />
+      <Note />
+    </div>
+
+    <div class="modal fade" ref="modalRef">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">New/Edit Event</h5>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" @click="saveEvent">Save</button>
+          </div>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
