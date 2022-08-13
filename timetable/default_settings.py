f11017989f29060b65f803afa855f08756db89fa
Ji ZHANG <zhangji87@gmail.com>
2022-08-13 16:43:50 +0800

    Fix pylint

--- timetable/default_settings.py
+++ timetable/default_settings.py
@@ -4,5 +4,6 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///../timetable.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 USERS = {
-    "test": "pbkdf2:sha256:150000$RQEmXgTI$39b2c288c2af0cc6380add124254364a910483acd0b8d1dc81260aa1649e99d5",
+    "test": "pbkdf2:sha256:150000$RQEmXgTI$39b2c288c2af0cc6380add124254364a910483acd0b8d1dc81260aa1"
+    "649e99d5",
 }
