ea700218870ba4c9f4577a6cea933fccde5c8108
Jerry <zhangji87@gmail.com>
2023-05-17 16:10:44 +0800

    Fix flask-sqlalchemy warning

--- timetable/default_settings.py
+++ timetable/default_settings.py
@@ -1,6 +1,7 @@
 SECRET_KEY = "secret key"
 
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/timetable"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 USERS = {
     "test": "pbkdf2:sha256:150000$RQEmXgTI$39b2c288c2af0cc6380add124254364a910483acd0b8d1dc81260aa1"
